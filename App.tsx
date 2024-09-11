1ecf7408f76d8c255630065b5955035ca30ca233
Emil Abdullayev <abdullayevemil27042006@gmail.com>
2024-09-11 16:05:53 +0400

    Add card management buttons

--- App.tsx
+++ App.tsx
@@ -1,135 +1,203 @@
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  View,
-  Image,
-  Pressable,
-  Text,
-  StatusBar
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Image,
+    Pressable,
+    Text,
+    StatusBar,
 } from 'react-native';
 
 const currentBalance = 7429.23;
 
+const MyStatusBar = ({backgroundColor, ...props}) => (
+    <View style={[styles.statusBar, {backgroundColor}]}>
+        <SafeAreaView>
+            <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+        </SafeAreaView>
+    </View>
+);
+
 export const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar
-        backgroundColor={'#3A2636'}
-        barStyle={'light-content'}
-      />
+    return (
+        <>
+            <MyStatusBar
+                backgroundColor='#3A2636'
+                barStyle='light-content'
+            />
+            <SafeAreaView style={styles.container}>
 
-      <View style={styles.headerContainer}>
-        <View style={styles.header}>
-          <View style={styles.profile}>
-            <View style={styles.galleryContainer}>
-              <Image style={styles.galleryImage} source={require('./assets/icons/gallery.png')} />
-            </View>
-            <View>
-              <Text style={styles.headerSubtitle}>Hello!</Text>
+                <View style={styles.headerContainer}>
+                    <View style={styles.header}>
+                        <View style={styles.profile}>
+                            <View style={styles.galleryContainer}>
+                                <Image style={styles.galleryImage} source={require('./assets/icons/gallery.png')}/>
+                            </View>
+                            <View>
+                                <Text style={styles.headerSubtitle}>Hello!</Text>
 
-              <Text style={styles.headerTitle}>Esther Howard</Text>
-            </View>
-          </View>
+                                <Text style={styles.headerTitle}>Esther Howard</Text>
+                            </View>
+                        </View>
 
-          <Pressable style={styles.walletButton}>
-            <Image source={require('./assets/icons/wallet.png')} />
-          </Pressable>
-        </View>
+                        <Pressable style={styles.walletButton}>
+                            <Image source={require('./assets/icons/wallet.png')}/>
+                        </Pressable>
+                    </View>
 
-        <View style={styles.balanceContainer}>
-          <View style={styles.balanceDetails}>
-            <Text style={styles.balanceTitle}>Current Balance</Text>
+                    <View style={styles.balanceContainer}>
+                        <View style={styles.balanceDetails}>
+                            <Text style={styles.balanceTitle}>Current Balance</Text>
 
-            <View style={styles.currentBalanceContainer}>
-              <Text style={styles.currency}>$</Text>
+                            <View style={styles.currentBalanceContainer}>
+                                <Text style={styles.currency}>$</Text>
 
-              <Text style={styles.currentBalance}>{currentBalance}</Text>
-            </View>
-          </View>
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+                                <Text style={styles.currentBalance}>{currentBalance}</Text>
+                            </View>
+                        </View>
+
+                        <View style={styles.cardManagementContainer}>
+                            <Pressable style={styles.cardManagementButton}>
+                                <Image style={styles.plusIcon} source={require('./assets/icons/plus-icon.png')}/>
+
+                                <Text style={styles.addNewCard}>Add New Card</Text>
+                            </Pressable>
+
+                            <Pressable style={styles.cardManagementButton}>
+                                <Image style={styles.plusIcon} source={require('./assets/icons/double-arrow.png')}/>
+
+                                <Text style={styles.addNewCard}>Change Card</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+
+                    <View style={styles.dragDownLine}/>
+                </View>
+            </SafeAreaView>
+        </>
+    );
 };
 
+const STATUSBAR_HEIGHT = StatusBar.currentHeight;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  headerContainer: {
-    backgroundColor: '#3A2636',
-    paddingHorizontal: 18,
-    gap: 42,
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: 48,
-  },
-  profile: {
-    flexDirection: 'row',
-    gap: 12,
-  },
-  galleryContainer: {
-    borderRadius: 9999,
-    backgroundColor: '#DADADA',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 44,
-    height: 44,
-  },
-  galleryImage: {
-    width: 24,
-    height: 24,
-  },
-  headerSubtitle: {
-    color: '#9c929b',
-    fontSize: 16,
-    fontFamily: 'Clash Grotesk',
-  },
-  headerTitle: {
-    color: 'white',
-    fontSize: 18,
-    fontFamily: 'Clash Grotesk',
-  },
-  walletButton: {
-    borderRadius: 9999,
-    borderColor: '#746772',
-    borderWidth: 2,
-    width: 48,
-    height: 48,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  balanceContainer: {
-    height: 183,
-    gap: 8,
-  },
-  balanceDetails: {
-    marginHorizontal: 14,
-    alignItems: 'center',
-    gap: 8,
-    borderBottomWidth: 1,
-    borderColor: '#60515e',
-    paddingBottom: 8,
-  },
-  balanceTitle: {
-    fontSize: 18,
-    color: '#9c929b',
-    fontFamily: 'Clash Grotesk',
-  },
-  currentBalanceContainer: {
-    flexDirection: 'row',
-  },
-  currency: {
-    fontSize: 48,
-    color: '#c4bec3',
-    fontFamily: 'Clash Grotesk',
-  },
-  currentBalance: {
-    fontSize: 48,
-    color: 'white',
-    fontFamily: 'Clash Grotesk',
-  },
+    container: {
+        flex: 1,
+        gap: 32,
+    },
+    headerContainer: {
+        backgroundColor: '#3A2636',
+        paddingHorizontal: 18,
+        gap: 42,
+        paddingBottom: 14,
+        borderBottomLeftRadius: 24,
+        borderBottomRightRadius: 24,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: 48,
+    },
+    profile: {
+        flexDirection: 'row',
+        gap: 12,
+    },
+    galleryContainer: {
+        borderRadius: 9999,
+        backgroundColor: '#DADADA',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 44,
+        height: 44,
+    },
+    galleryImage: {
+        width: 24,
+        height: 24,
+    },
+    headerSubtitle: {
+        color: '#9c929b',
+        fontSize: 16,
+        fontFamily: 'Clash Grotesk',
+    },
+    headerTitle: {
+        color: 'white',
+        fontSize: 18,
+        fontFamily: 'Clash Grotesk',
+    },
+    walletButton: {
+        borderRadius: 9999,
+        borderColor: '#746772',
+        borderWidth: 2,
+        width: 48,
+        height: 48,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    balanceContainer: {
+        gap: 24,
+        alignItems: 'center',
+    },
+    balanceDetails: {
+        alignItems: 'center',
+        gap: 8,
+        borderBottomWidth: 1,
+        borderColor: '#60515e',
+        paddingBottom: 8,
+    },
+    balanceTitle: {
+        fontSize: 18,
+        color: '#9c929b',
+        fontFamily: 'Clash Grotesk',
+    },
+    currentBalanceContainer: {
+        flexDirection: 'row',
+    },
+    currency: {
+        fontSize: 48,
+        lineHeight: 59,
+        color: '#c4bec3',
+        fontFamily: 'Clash Grotesk',
+    },
+    currentBalance: {
+        fontSize: 48,
+        lineHeight: 59,
+        color: 'white',
+        fontFamily: 'Clash Grotesk',
+    },
+    plusIcon: {
+        width: 18,
+        height: 18,
+    },
+    cardManagementContainer: {
+        flexDirection: 'row',
+        gap: 12,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    cardManagementButton: {
+        borderRadius: 12,
+        flexDirection: 'row',
+        paddingHorizontal: 24,
+        paddingVertical: 14,
+        gap: 12,
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    },
+    addNewCard: {
+        fontSize: 14,
+        color: 'white',
+    },
+    dragDownLine: {
+        width: 64,
+        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+        borderWidth: 2,
+        borderColor: 'rgba(255, 255, 255, 0.5)',
+        borderRadius: 9999,
+        alignSelf: 'center',
+    },
+    statusBar: {
+        height: STATUSBAR_HEIGHT,
+        backgroundColor: '#3A2636',
+        paddingBottom: 32,
+    },
 });
